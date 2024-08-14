6c7e489835774c50656cd2bbefbb88f4cc954d1d
quan2910 <ngtruongquan@gmail.com>
2024-08-14 15:46:57 +0700

    update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -22,32 +22,26 @@ const Header = () => {
           </Link>
           <HeaderSearch />
           <div className="flex min-w-[270px] items-center">
-            <Button
-              variant="ghost"
-              className="flex p-0 items-center gap-[10px] mr-[26px]"
-            >
+            <div className="flex p-0 cursor-pointer items-center gap-[10px] mr-[26px]">
               <Image src={user} alt="user" />
               <div className="flex flex-col items-start justify-start">
                 <p className="text-11 text-secondary-1">Login</p>
                 <p className="text-14">Account</p>
               </div>
-            </Button>
-            <Button variant="ghost" className="relative p-0 mr-[35px]">
+            </div>
+            <div className="relative cursor-pointer p-0 mr-[35px]">
               <Image src={heart} alt="heart" />
-              <div className="rounded-full absolute top-[0px] -right-[6px] bg-red size-[18px] flex items-center justify-center text-white text-10">
+              <div className="rounded-full absolute -top-[6px] -right-[6px] bg-red size-[18px] flex items-center justify-center text-white text-10">
                 2
               </div>
-            </Button>
-            <Button
-              variant="ghost"
-              className="flex items-center p-0 gap-[10px]"
-            >
+            </div>
+            <div className="flex cursor-pointer items-center p-0 gap-[10px]">
               <CartIcon />
               <div>
                 <p className="text-11 text-secondary-1">Your Cart</p>
                 <p className="text-14">$280.00</p>
               </div>
-            </Button>
+            </div>
           </div>
         </div>
         <HeaderNavigation />
